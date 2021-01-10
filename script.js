be60a8d1a3ffb04e9e8234641e6b872ef3176843
SetiNgabo45 <mapito.ngabo@ocsbstudent.ca>
2021-01-10 01:48:51 +0000

    L4 l6 L10

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function solveArea() {
-  let width= document.getElementById("width").value;
+  let width= document.getElementById('width').value;
   width= parseInt(width);
-  let length= document.getElementById("length").value;
+  let length= document.getElementById('length').value;
   length= parseInt(length);
-  let height= document.getElementById("height").value;
+  let height= document.getElementById('height').value;
   height= parseInt(height)
-  let area = width+length*height/2;
-  document.getElementById("area").value = area;
+  let area = length + width * height / 2;
+  area= parseInt(area);
+  document.getElementById('area').value = area;
 }
