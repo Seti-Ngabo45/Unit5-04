bec64606a39d7938a95e2f4f307666bf2c223820
SetiNgabo45 <mapito.ngabo@ocsbstudent.ca>
2021-01-10 02:36:04 +0000

    js:2:12 js:3:8 js:4:13 js:5:9 js:6:13 js:7:9 js:9:7 js:11:2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function solveArea() {
+function solve() {
   let width= document.getElementById('width').value;
-  width= parseInt(width);
-  let length= document.getElementById('length').value;
-  length= parseInt(length);
-  let height= document.getElementById('height').value;
-  height= parseInt(height)
+  width = parseInt(width);
+  let length = document.getElementById('length').value;
+  length = parseInt(length);
+  let height = document.getElementById('height').value;
+  height = parseInt(height)
   let area = length + width * height / 2;
-  area= parseInt(area);
+  area = parseInt(area); 
   document.getElementById('area').value = area;
 }
