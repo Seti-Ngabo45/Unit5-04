25c605b1eff9d12462647c30c09b02197893a756
SetiNgabo45 <mapito.ngabo@ocsbstudent.ca>
2021-01-10 03:00:14 +0000

    js:1:15 js:1:15

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function solve() {
-  let width= document.getElementById('width').value;
-  width = parseInt(width);
-  let length = document.getElementById('length').value;
-  length = parseInt(length);
-  let height = document.getElementById('height').value;
+function solve () {
+  let width = document.getElementById('width').value
+  width = parseInt(width)
+  let length = document.getElementById('length').value
+  length = parseInt(length)
+  let height = document.getElementById('height').value
   height = parseInt(height)
-  let area = length + width * height / 2;
-  area = parseInt(area); 
-  document.getElementById('area').value = area;
+  let area = length + width * height / 2
+  area = parseInt(area)
+  document.getElementById('area').value = area
 }
